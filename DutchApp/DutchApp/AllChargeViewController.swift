695a9af67846a514519682123e68d8a8c992865a
전상민 <jeonsangmin77@gmail.com>
2020-01-15 16:30:31 +0900

    second commit sangmin

---
+++ DutchApp/DutchApp/AllChargeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AllChargeViewController.swift
+//  DutchApp
+//
+//  Created by MyMac on 2020/01/15.
+//  Copyright © 2020 Team Dutch. All rights reserved.
+//
+
+import UIKit
+
+class AllChargeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
