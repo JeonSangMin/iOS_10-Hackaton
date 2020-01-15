892cf643793cb8bc651a16e49654b1fce008e8d0
hongdonghyun <hong4367@gmail.com>
2020-01-15 14:27:39 +0900

    fix gitignore

---
+++ DutchApp/DutchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DutchApp
+//
+//  Created by Hongdonghyun on 2020/01/15.
+//  Copyright © 2020 Team Dutch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
